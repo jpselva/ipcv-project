97fa745030fcfbe195762af0b80357e04a04d31a
JPS <jpsb@duck.com>
2024-10-26 20:42:25 +0200

    Add triangulation function

---
+++ triangulation.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+
+def triangulate_points(pts1, pts2, R, T):
+    """find 3D points from two lists of 2D image points of stereo cameras. The
+    coordinate system of the first camera is used as the origin
+
+    Arguments:
+    pts1 -- list of image points seen from 1st camera
+    pts2 -- list of image points seen from 2nd camera
+    R -- rotation matrix to transform from 2nd camera CS to 1st camera CS (see stereo_calibrate)
+    T -- translation to transform from 2nd camera CS to 1st camera CS (see stereo_calibrate)
+    """
+    P1 = np.hstack([np.eye(3), np.zeros((3, 1))])
+    R_inv = np.linalg.inv(R)
+    T_inv = -np.matmul(R_inv, T)
+    P2 = np.hstack([R_inv, -T_inv])
+
+    points = cv.triangulatePoints(P1, P2, pts1, pts2).T
+    points = np.array([p / p[3] for p in points])
+
+    return points
